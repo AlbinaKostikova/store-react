a32663c4dd3176b8d6d78fb586e020b52eabfd99
AlbinaKostikova <albucit33@gmail.com>
2025-11-13 14:43:50 +0600

    cards and cart added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,64 @@
+import React from "react"
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-function App() {
-  return (
+import Items from "./components/Items";
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        {
+          id: 1,
+          title: 'Стул серый',
+          img: 'chair.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'chairs',
+          price: '49.99'
+        },
+        {
+          id: 2,
+          title: 'Стол',
+          img: 'table.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'tables',
+          price: '149.00'
+        },
+        {
+          id: 3,
+          title: 'Диван',
+          img: 'sofa.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'sofas',
+          price: '49.99'
+        },
+        {
+          id: 4,
+          title: 'Лампа',
+          img: 'lamp.jpg',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'lamps',
+          price: '49.99'
+        },
+        {
+          id: 5,
+          title: 'Кресло',
+          img: 'arm-chair.webp',
+          desc: 'Lorem ipsum dolor sit amet, consectetur adipisicing.',
+          category: 'chairs',
+          price: '49.99',
+        }
+      ]
+    }
+  }
+  render() {
+    return (
     <div className='wrapper'>
       <Header/>
+      <Items items={this.state.items}/>
       <Footer/>
     </div>
   )
+}
 }
 export default App;
